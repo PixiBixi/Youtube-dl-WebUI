b9ced0762c81d62b6c28e0b240f9cb1134c81711
PixiBixi <PixiBixi@users.noreply.github.com>
2017-02-25 21:08:57 +0100

    [FIX] Replace avconv by ffmpeg to improve compatibility

--- config/config.php
+++ config/config.php
@@ -9,7 +9,7 @@ $config = [
     "security"     => true,
     "password"     => "4813494d137e1631bba301d5acab6e7bb7aa74ce1185d456565ef51d737677b2",
     "outputFolder" => "downloads",
-    "extracter"    => "avconv",
+    "extracter"    => "ffmpeg",
     "max_dl"       => 3,
 ];
 return $config;
